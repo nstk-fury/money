7464b66df8c351217d3862adaca19744d6c2eb22
Logqwerty <meg0070@naver.com>
2019-10-16 16:17:01 +0900

    [feature] 4장 완료
    [x] amount를 private으로 만들기
    
    js에서는 private 제어자가 없으므로 테스트 코드를 수정하는 것으로 마무리.

--- test/money.test.js
+++ test/money.test.js
@@ -2,10 +2,8 @@ const { Dollar } = require("../src/dollar");
 
 test("Multiplication 테스트", () => {
   const five = new Dollar(5);
-  let product = five.times(2);
-  expect(product.amount).toEqual(10);
-  product = five.times(3);
-  expect(product.amount).toEqual(15);
+  expect(five.times(2)).toEqual(new Dollar(10));
+  expect(five.times(3)).toEqual(new Dollar(15));
 });
 
 test("Equality 테스트", () => {
