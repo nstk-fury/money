546e82fd2dbd70669ce2c9eb3ddb8188173e17ee
doong-jo <sdong001@gmail.com>
2019-10-16 16:47:58 +0900

    4장: 프라이버시

--- dollar.spec.js
+++ dollar.spec.js
@@ -3,14 +3,11 @@ import Dollar from "./dollar";
 describe("Dollar Example", () => {
     test("testMultiplication", () => {
         const five = new Dollar(5);
-        let proudct = five.times(2);
-        expect(10 === proudct.amount).toBeTruthy();
-        proudct = five.times(3);
-        expect(15 === proudct.amount).toBeTruthy();
+        expect(new Dollar(10).equals(five.times(2))).toBeTruthy();
+        expect(new Dollar(15).equals(five.times(3))).toBeTruthy();
     });
 
     test("testEquality", () => {
         expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-        expect(new Dollar(5).equals(new Dollar(6))).toBeTruthy();
     });
 });
