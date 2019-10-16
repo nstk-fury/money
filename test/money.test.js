9045635c34fe6382c4e2dddcdfb7f2c7d4b8ea8b
Logqwerty <meg0070@naver.com>
2019-10-16 15:51:14 +0900

    [feature] 2장 구현
    [x] Dollar 부작용

--- test/money.test.js
+++ test/money.test.js
@@ -2,6 +2,8 @@ const { Dollar } = require("../src/dollar");
 
 test("Multiplication 테스트", () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toEqual(10);
+  let product = five.times(2);
+  expect(product.amount).toEqual(10);
+  product = five.times(3);
+  expect(product.amount).toEqual(15);
 });
