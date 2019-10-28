23502648193a3144659e9656696c7afa5e80c89d
jominjimail <jominjimail@hanyang.ac.kr>
2019-10-28 22:27:15 +0900

    10장. 흥미로운 시간
    두 times()를 일치시키기 위해 그 메서드들이 호출하는 다른 메서드들을
    인라인시키 후 상수를 변수로 바꿔줌
    단지 디버깅을 위해 테스트 없이 toString()을 작성

--- src/main/wycash/Money.java
+++ src/main/wycash/Money.java
@@ -1,6 +1,6 @@
 package wycash;
 
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
 
@@ -12,7 +12,7 @@ abstract class Money {
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
 
     }
 
@@ -25,9 +25,16 @@ abstract class Money {
         return new Franc(amount, "CHF");
     }
 
-    abstract Money times (int multiplier);
+    Money times(int multiplier){
+        return new Money(amount*multiplier, currency);
+    }
 
     String currency(){
         return  currency;
     };
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
 }
--- src/test/wycash/DollarTest.java
+++ src/test/wycash/DollarTest.java
@@ -36,4 +36,9 @@ public class DollarTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
+    public void testDifferentClassEquality() {
+        assertTrue(new Money(10, "CHF").equals(new Franc(10,"CHF")));
+    }
+
 }
