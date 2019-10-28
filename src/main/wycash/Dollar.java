b50707f2674dc21bc16a27d19d19d06ea343f1ee
jominjimail <jominjimail@hanyang.ac.kr>
2019-10-28 15:43:38 +0900

    6장. 돌아온 '모두를 위한 평등'
    공통된 코드를 첫 번째 클래스(Dollar) 에서 상위 클래스 (Money)로
    옮김 두번째 클래스(Franc)도 Money의 하위 클래스로 만듦
    불필요한 구현을 제거하기 전에 두 equals()구현을 일치시킴

--- src/main/wycash/Dollar.java
+++ src/main/wycash/Dollar.java
@@ -1,7 +1,6 @@
 package wycash;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -11,9 +10,4 @@ public class Dollar {
         return new Dollar(amount*multiplier);
     }
 
-    public boolean equals(Object object){
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-
-    }
 }
--- src/main/wycash/Franc.java
+++ src/main/wycash/Franc.java
@@ -1,7 +1,6 @@
 package wycash;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money{
 
     Franc(int amount) {
         this.amount = amount;
@@ -9,11 +8,6 @@ public class Franc {
 
     Franc times(int multiplier){
         return new Franc(amount*multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 
 }
