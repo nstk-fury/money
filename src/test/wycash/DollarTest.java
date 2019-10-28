0b41599ad487a5bb8563bca652654213f3a28c41
jominjimail <jominjimail@hanyang.ac.kr>
2019-10-28 23:31:27 +0900

    11장. 모든 악의 근원
    하위 클래스의 속을 들어냈고 하위 클래스를 더이상 사용하지 않음
    필요없어진 테스트도 삭제함

--- src/test/wycash/DollarTest.java
+++ src/test/wycash/DollarTest.java
@@ -16,9 +16,6 @@ public class DollarTest {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
 
     }
@@ -34,11 +31,6 @@ public class DollarTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10,"CHF")));
     }
 
 }
