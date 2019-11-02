30db29d703afc91ad4dcf9890066cbb8675adf23
Logqwerty <meg0070@naver.com>
2019-11-02 13:58:15 +0900

    2장 완료
    [x] Dollar side effect

--- test/money.test.ts
+++ test/money.test.ts
@@ -2,6 +2,8 @@ import { Dollar } from "../src/dollar";
 
 test("Multiplication 테스트", () => {
   const five: Dollar = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toEqual(10);
+  let product: Dollar = five.times(2);
+  expect(product.amount).toEqual(10);
+  product = five.times(3);
+  expect(product.amount).toEqual(15);
 });
