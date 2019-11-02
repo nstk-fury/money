69104d425bb96e6d0dbb8c79663ad7e67c040880
Logqwerty <meg0070@naver.com>
2019-11-02 14:09:52 +0900

    4장 완료
    [x] Make "amount" private4장 완료

--- test/money.test.ts
+++ test/money.test.ts
@@ -2,10 +2,8 @@ import { Dollar } from "../src/dollar";
 
 test("Multiplication 테스트", () => {
   const five: Dollar = new Dollar(5);
-  let product: Dollar = five.times(2);
-  expect(product.amount).toEqual(10);
-  product = five.times(3);
-  expect(product.amount).toEqual(15);
+  expect(five.times(2)).toEqual(new Dollar(10));
+  expect(five.times(3)).toEqual(new Dollar(15));
 });
 
 test("Equality 테스트", () => {
