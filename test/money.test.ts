64f00caae80e55a42f0d939ad1bdd7eb7a7dac90
Logqwerty <meg0070@naver.com>
2019-11-02 16:22:02 +0900

    10장 완료
    [x] Delete testFrancMultiplication

--- test/money.test.ts
+++ test/money.test.ts
@@ -14,12 +14,6 @@ test("Equality 테스트", () => {
   expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
 });
 
-test("Franc Multiplication 테스트", () => {
-  const five: Money = Money.franc(5);
-  expect(five.times(2)).toEqual(Money.franc(10));
-  expect(five.times(3)).toEqual(Money.franc(15));
-});
-
 test("Currency 테스트", () => {
   expect(Money.dollar(1).currency()).toEqual("USD");
   expect(Money.franc(1).currency()).toEqual("CHF");
